4a0dbf9d73e61798e84a95eb7a188636117ad1e1
RBuczeskie <ramseyjbuczeskie47@gmail.com>
2025-03-27 14:58:58 -0400

    camera test exercise upload

---
+++ exercises/CameraTest/app/src/main/java/com/example/cameratest/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.cameratest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
+    BitmapDrawable drawable;
+    RequestQueue queue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button capture = findViewById(R.id.camera_button);
+        imageView = findViewById(R.id.cameraImageView);
+
+        queue = Volley.newRequestQueue(this);
+        queue.start();
+
+
+
+        capture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("CameraTest","in Click");
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+
+        Button list = findViewById(R.id.list_button);
+        list.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d("CameraTest","in list Click");
+                Intent i = new Intent(MainActivity.this, ViewActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                assert data != null;
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                imageView.setImageBitmap(imageBitmap);
+
+                drawable = (BitmapDrawable) imageView.getDrawable();
+                final Bitmap bitmap = drawable.getBitmap();
+
+                uploadToServer(encodeToBase64(bitmap,Bitmap.CompressFormat.PNG,100));
+
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "You cancelled the operation", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public static String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat, int quality) {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
+    }
+
+    private void uploadToServer(final String image) {
+
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("store", "amz");
+            json.put("image", image);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url = "http://10.2.106.60:5000/image";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, json,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Hello", "Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Hello", error.getMessage());
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+    }
+}
---
+++ exercises/CameraTest/mongo_app.py
@@ -0,0 +1,80 @@
+import json
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from flask import Flask
+from flask import request
+from flask.json import jsonify
+import certifi
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://ramseyjbuczeskie47:34035605@cisc349.aqymd.mongodb.net/?retryWrites=true&w=majority&appName=CISC349', tlsCAFile=certifi.where())
+
+db = client["CISC349"]
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server!!</h1>"
+
+#Add user
+@app.route('/add', methods=['POST'])
+def add():
+#def add(name, address):
+    collection = db["customers"]
+    request_data = request.get_json()
+    
+    name = request_data['name']
+    address = request_data['address']
+    phone = request_data['phone']
+    
+    print(f'Name: {name}, Address: {address}, Phone: {phone}')
+    #data = {"name":name, "address":address}
+    _id = collection.insert_one(request_data)
+    return json.dumps({'id':str(_id.inserted_id)})
+    
+#Update user
+@app.route('/update', methods=['POST'])
+def update():
+    collection = db["customers"]
+    request_data = request.get_json()
+    
+    id = request_data['_id']
+    name = request_data['name']
+    address = request_data['address']
+    phone = request_data['phone']
+    comments = request_data['comments']
+    
+    print(f'ID: {id}, Name: {name}, Address: {address}, Phone: {phone}, Comments: {comments}')
+    filter = {'_id':id}
+    newvalues = {"$push": {'comments':comments}}
+    
+    _id = collection.update_one(filter, newvalues)
+    return json.dumps({'_id':ObjectId(id)})
+	
+#Select all users
+#@app.route('/all', methods=['POST'])
+@app.route('/all', methods=['GET'])
+def all():
+    collection = db["customers"]
+    customers = list(collection.find())
+    print(customers)
+    #convert _id to str
+    return json.dumps(customers, default=str)
+	
+@app.route("/image", methods=["POST"])
+def image_save():
+    collection = db["images"]
+    content = request.get_json()
+    _id = collection.insert_one(content)
+    return json.dumps({'id' : str(_id.insterted_id)})
+    
+@app.route('/images', methods=['GET'])
+def image_list():
+    collection = db["images"]
+    images = list(collection.find())
+    # convert _id to str
+    print(f"Got {len(images)} images.")
+    return json.dumps(images, default=str)
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
